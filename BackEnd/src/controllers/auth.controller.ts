2e86fee7902accb46b0e56b12a4603cd6974ac23
Uriel <urielrre@gmail.com>
2025-07-16 23:54:39 -0400

    refactor idea...

--- BackEnd/src/controllers/auth.controller.ts
+++ BackEnd/src/controllers/auth.controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from "express";
 import UserService from "../services/user.service";
+import { LoginUser } from "../schemas/user.schema";
 
 class AuthController {
   private readonly userService = new UserService();
 
-  async handleLogin(req: Request, res: Response): Promise<any> {}
+  async handleLogin(req: Request, res: Response): Promise<any> {
+    const body: LoginUser = req.body;
+    try {
+      return await this.userService.login(body);
+    } catch (error) {
+      res
+        .status(500)
+        .send(error instanceof Error ? error.message : "Unknown error");
+    }
+  }
   async handleCreateUser(req: Request, res: Response): Promise<any> {}
   async handleUpdateUser(req: Request, res: Response): Promise<any> {}
   async handleDeleteUser(req: Request, res: Response): Promise<any> {}
