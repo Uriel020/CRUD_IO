d809c6bd038513269d8fb3837eb7ecf810d772f5
Uriel <urielrre@gmail.com>
2025-08-02 23:34:15 -0400

    add authRoute

--- BackEnd/src/routes/auth.route.ts
+++ BackEnd/src/routes/auth.route.ts
@@ -1,12 +1,18 @@
 import {Router} from 'express';
+import { validatorSchema } from '../middlewares/zodValidator.middleware';
+import { createUser } from '../schemas/user.schema';
+import { SchemaType } from '../types/schemaType';
+import AuthController from '../controllers/auth.controller';
 
 const router:Router = Router();
+const {handleLogin, handleRegisterUser, handleSoftDeleteUser, handleUpdateUser} = new AuthController();
 
-router.post('');
-router.post('');
-router.post('');
-router.put('');
-router.delete('');
+
+router.post('/register', validatorSchema(createUser, SchemaType.body), handleRegisterUser);
+router.post('/login' );
+router.get('profile');
+router.put('user/:id');
+router.delete('user/:id');
 
 
 
