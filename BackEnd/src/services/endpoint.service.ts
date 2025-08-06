8fc0855e4bb215bde56880f0ce0d51ca658153c7
Uriel <urielrre@gmail.com>
2025-08-06 17:14:22 -0400

    enpoint service ready

--- BackEnd/src/services/endpoint.service.ts
+++ BackEnd/src/services/endpoint.service.ts
@@ -1,4 +1,8 @@
 import EndpointRepository from "../repositories/endpoint.repository";
+import {
+  CreateEndpointDTO,
+  UpdateEndpointDTO,
+} from "../schemas/endpoint.schema";
 import { Endpoint } from "../types/endpoint";
 import { FindType } from "../types/findType";
 import { validate } from "../utils/validate";
@@ -7,10 +11,24 @@ class EndpointService {
   constructor(private readonly endpointRepo = new EndpointRepository()) {}
 
   async getEndpointOwnedByResource(
-    idResource: string
+    idEndpoint: string
   ): Promise<Endpoint | null> {
-    await validate(idResource, FindType.endpoint);
-    return this.endpointRepo.findById(idResource);
+    await validate(idEndpoint, FindType.endpoint);
+    return this.endpointRepo.findById(idEndpoint);
+  }
+  async createEndpoint(body: CreateEndpointDTO): Promise<Endpoint> {
+    return this.endpointRepo.create(body);
+  }
+  async modifyEndpoint(
+    idEndpoint: string,
+    body: UpdateEndpointDTO
+  ): Promise<Endpoint> {
+    await validate(idEndpoint, FindType.endpoint);
+    return this.endpointRepo.update(idEndpoint, body);
+  }
+  async deleteEndpoint(idEndpoint: string): Promise<Endpoint> {
+    await validate(idEndpoint, FindType.endpoint);
+    return this.endpointRepo.delete(idEndpoint);
   }
 }
 
