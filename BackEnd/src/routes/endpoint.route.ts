a0139f97f91f5107a90e6735961231e576aea99d
Uriel <urielrre@gmail.com>
2025-08-09 09:40:06 -0400

    endpoint route ready

--- BackEnd/src/routes/endpoint.route.ts
+++ BackEnd/src/routes/endpoint.route.ts
@@ -1,14 +1,40 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/zodValidator.middleware";
-import { createEndpoint, endpointParams } from "../schemas/endpoint.schema";
+import {
+  createEndpoint,
+  endpointParams,
+  updateEndpoint,
+} from "../schemas/endpoint.schema";
 import { SchemaType } from "../types/schemaType";
+import EndpointController from "../controllers/endpoint.controller";
 
 const router: Router = Router();
 
+const {
+  handleCreateEndpoint,
+  handleDeleteEndpoint,
+  handleGetEndpoint,
+  handleUpdateEndpoint,
+} = new EndpointController();
+
 router.get(
   "/endpoint/:id",
-  validateSchema(endpointParams, SchemaType.params)
+  validateSchema(endpointParams, SchemaType.params),
+  handleGetEndpoint
 );
-router.post("/endpoint", validateSchema(createEndpoint, SchemaType.body));
-router.put("/endpoint/:id", );
-router.delete("/endpoint/:id");
+router.post(
+  "/endpoint",
+  validateSchema(createEndpoint, SchemaType.body),
+  handleCreateEndpoint
+);
+router.put(
+  "/endpoint/:id",
+  validateSchema(endpointParams, SchemaType.params),
+  validateSchema(updateEndpoint, SchemaType.body),
+  handleUpdateEndpoint
+);
+router.delete(
+  "/endpoint/:id",
+  validateSchema(endpointParams, SchemaType.params),
+  handleDeleteEndpoint
+);
